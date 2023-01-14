3be1b70ec568bfc8154453ac3223d1a96ef3034e
MSI\User <tmadusanka333@gmail.com>
2023-01-14 13:52:09 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
+import {MatTableDataSource} from '@angular/material/table';
+
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+// ];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crudapp';
+  dataSource!: MatTableDataSource<any>;
+  constructor(private dialog: MatDialog, private api: ApiService) { }
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent,
+      {
+        width: '30%'
+
+      });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  getAllProducts(){
+    this.api.getProduct()
+    .subscribe({
+      next:(res)=>{
+        // console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+
+
+      },
+      error:(err)=>{
+        alert("Error while fetching the records!!");
+      }
+    })
+  }
+
+  displayedColumns: string[] = ['id','productName', 'category', 'freshness', 'price','date','comment','action'];
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  editProduct(row :any){
+    this.dialog.open(DialogComponent,{
+      width:'30%',
+      data:row
+    })
+    
+  }
 }
---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,93 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { MatDialogRef,MAT_DIALOG_DATA } from '@angular/material/dialog';
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+
+
+export class DialogComponent {
+
+
+  foods: Food[] = [
+    { value: 'Steak', viewValue: 'Steak' },
+    { value: 'Pizza', viewValue: 'Pizza' },
+    { value: 'Tacos', viewValue: 'Tacos' },
+  ];
+
+  freshnessList: string[] = ['Brand New', 'Second Hand', 'Refurbished'];
+  productForm!: FormGroup;
+  actioBtn :string="Save";
+  constructor(private formBuilder: FormBuilder, private api: ApiService ,
+    @Inject(MAT_DIALOG_DATA) public editData :any,
+     private dialogRef : MatDialogRef<DialogComponent>) { }
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      category: ['', Validators.required],
+      freshness: ['', Validators.required],
+      price: ['', Validators.required],
+      comment: ['', Validators.required],
+      date: ['', Validators.required]
+
+    });
+    if(this.editData){
+      this.actioBtn="Update";
+      this.productForm.controls['productName'].setValue(this.editData.productName);
+      this.productForm.controls['category'].setValue(this.editData.category);
+      this.productForm.controls['freshness'].setValue(this.editData.freshness);
+      this.productForm.controls['price'].setValue(this.editData.price);
+      this.productForm.controls['comment'].setValue(this.editData.comment);
+      this.productForm.controls['date'].setValue(this.editData.date);
+
+
+    }
+
+    console.log(this.editData);
+  }
+  addProduct() {
+    // console.log(this.productForm.value);
+    if(!this.editData){
+      if (this.productForm.valid) {
+        this.api.postProduct(this.productForm.value)
+        .subscribe({
+          next:(res)=>{
+            alert("Product added successfully...!");
+            this.productForm.reset();
+            this.dialogRef.close('saved');
+          },
+          error:()=>{
+            alert("Error while adding the product!!!!")
+          }
+        })
+      }
+    }else{
+      this.updateProduct()
+    }
+  }
+
+updateProduct(){
+  this.api.putProduct(this.productForm.value,this.editData.id)
+  .subscribe({
+    next:(res)=>{
+      alert("Product update successfuly..!!");
+      this.productForm.reset();
+      this.dialogRef.close('update');
+    },
+    error:()=>{
+      alert("Error while updating the record !");
+      }
+  })
+}
+
+
+}
